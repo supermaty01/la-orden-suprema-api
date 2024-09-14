c49a842dd9227c7e30d964f1c7220f9c7892abf8
Mateo Álvarez <mateo@netprogroup.com>
2024-09-13 20:44:46 -0500

    feat: add CORS config

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,X-Requested-With,Origin,Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use('', indexRouter);
 app.use('/admins', adminsRouter);
 app.use('/assassins', assassinsRouter);
